da1fcdcb2146af877091a94638fd96686fd0acc6
syarifuddin <syarifuddinsakri@gmail.com>
2026-09-07 21:51:28 +0800

    Added <leader>gc to quickly copy the commit hash of line where the cursor is located at

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -8,6 +8,29 @@ end, {
     desc = "[G]it [B]lame line",
 })
 
+vim.keymap.set("n", "<leader>gc", function()
+    local line = vim.fn.line(".")
+    local file = vim.fn.expand("%:p")
+
+    local sha = vim.fn.system({
+        "git",
+        "-C", vim.fn.fnamemodify(file, ":h"),
+        "blame",
+        "-L", line .. "," .. line,
+        "--porcelain",
+        vim.fn.fnamemodify(file, ":t"),
+    }):match("^([0-9a-f]+)")
+
+    if sha then
+        vim.fn.setreg("+", sha)
+        print("Copied: " .. sha)
+    else
+        print("Could not get commit SHA")
+    end
+end, {
+    desc = '[G]it copy [C]ommit Hash',
+})
+
 local gs = require("gitsigns")
 
 vim.keymap.set("n", "]h", gs.next_hunk, {desc = "Next Git Hunk"})
